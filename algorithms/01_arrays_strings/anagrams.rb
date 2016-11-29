b55d80a95b2b3abc058515d02d5259a62adc6e0d
Jonathan Pham <jon.pham@gmail.com>
2016-11-28 18:06:14 -0800

    Add initial exercise files.

---
+++ algorithms/01_arrays_strings/anagrams.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+# Write a method to decide if two strings are anagrams or not
+
+class AnagramChecker
+  def isAnagram(first_string,second_string)
+    return false
+  end
+end
+
+# Test Driver
+
+RSpec.describe AnagramChecker
+
